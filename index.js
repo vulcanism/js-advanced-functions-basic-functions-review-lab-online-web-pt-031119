10e4d5c15b41c668ba000954c1a104d625480310
Savannah C <sgcorrell@gmail.com>
2019-10-22 20:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function saturdayFun(activity="roller-skate") {
   return (`This Saturday, I want to ${activity}!`)
 }
+
+const mondayWork = (work="go to the office") => {
+  return (`This Monday, I will ${work}.`)
+}
